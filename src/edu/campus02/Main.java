4bfd80f36d4e67448b090de6b94a0ed02a5b68e0
Leo <haas.leonie.1@gmail.com>
2020-03-05 11:38:25 +0100

    Initial commit

---
+++ src/edu/campus02/Main.java
@@ -0,0 +1,21 @@
+package edu.campus02;
+
+import javax.print.DocFlavor;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        schreibe ("Text");
+    }
+
+    public static void schreibe(String text) {
+        System.out.println(text);
+        if (text.equals("text")) {
+        }else{
+            System.out.println("haha");
+        }
+    }
+
+
+}
